601e34f88a0d096dc73a10d7be786cf663009fdd
hugoregnier <hugoregnier7@gmail.com>
2021-07-23 17:31:11 +0200

    work configuration

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,39 @@
 const express = require('express');
-
 const app = express();
 
+const helmet = require("helmet");
+app.use(helmet());
 
+
+  // Ces headers permettent :
+  // d'accéder à notre API depuis n'importe quelle origine ( '*' ) 
+  // d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) 
+  // d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
 
 module.exports = app;
