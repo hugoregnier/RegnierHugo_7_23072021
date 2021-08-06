0b744bc59f10e69464d326ae8235851353ab1a5a
hugoregnier <hugoregnier7@gmail.com>
2021-08-06 17:44:43 +0200

    add async + get profile + put profile with update

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,11 +2,12 @@
 const bcrypt = require('bcrypt');
 const jwtUtils = require('../utils/jwt.utils');
 const models = require('../models');
+const asyncLib = require('async');
 
 
 // Regex ----------------
-const EMAIL_REGEX     = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const PASSWORD_REGEX  = /^(?=.*\d).{4,8}$/;
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
 
 
 // Nos fonctions register, login et recup ---------------------
@@ -24,85 +25,172 @@ module.exports = {
 
         if (username.length >= 13 || username.length <= 4) {
             return res.status(400).json({ 'erreur': 'Le nom d`utilisateur doit être compris entre 5 et 12 caractères' });
-          }
-      
-          if (!EMAIL_REGEX.test(email)) {
+        }
+
+        if (!EMAIL_REGEX.test(email)) {
             return res.status(400).json({ 'erreur': 'email non valide' });
-          }
-      
-          if (!PASSWORD_REGEX.test(password)) {
+        }
+
+        if (!PASSWORD_REGEX.test(password)) {
             return res.status(400).json({ 'erreur': 'mot de passe invalide, 4 à 8 caractères nécessaire avec 1 chiifre à la fin' });
-          }
+        }
 
-        // TODO verify pseudo length, mail regexp, password etc.
-
-        models.User.findOne({
-            attributes: ['email'],
-            where: { email: email }
-        })
-            .then(function (userFound) {
+        asyncLib.waterfall([
+            function (done) {
+                models.User.findOne({
+                    attributes: ['email'],
+                    where: { email: email }
+                })
+                    .then(function (userFound) {
+                        done(null, userFound);
+                    })
+                    .catch(function (err) {
+                        return res.status(500).json({ 'erreur': 'impossible de vérifier l`utilisateur' });
+                    });
+            },
+            function (userFound, done) {
                 if (!userFound) {
                     bcrypt.hash(password, 5, function (err, bcryptedPassword) {
-                        let newUser = models.User.create({
-                            email: email,
-                            username: username,
-                            password: bcryptedPassword,
-                            bio: bio,
-                            isAdmin: 0
-                        })
-                            .then(function (newUser) {
-                                return res.status(201).json({
-                                    'userID': newUser.id
-                                });
-                            })
-                            .catch(function(err) {
-                                return res.status(500).json({ 'erreur': 'vous ne pouvez pas ajouter d`utlisateur' });
-                            });
+                        done(null, userFound, bcryptedPassword);
                     });
                 } else {
                     return res.status(409).json({ 'erreur': 'l`utilisateur existe déjà' });
                 }
-            })
-    },
-
-
-    login: function (req, res) {
-
-        let email = req.body.email;
-        let password = req.body.password;
-
-        if (email == null || password == null) {
-            return res.status(400).json({ 'erreur': 'Il manque des parametres' });
-        }
-
-        // TODO verify pseudo length, mail regexp, password etc.
-
-
-
-        models.User.findOne({
-            where: { email: email }
-        })
-        .then(function (userFound) {
-            if (userFound) {
-                bcrypt.compare(password, userFound.password, function(errBcrypt, resBycrypt) {
-                    if (resBycrypt) {
-                        return res.status(200).json ({
-                            'userID': userFound.id,
-                            'token': jwtUtils.generateTokenForUser(userFound)
-                        });
-                    } else {
-                        return res.status(403).json ({ 'erreur': 'Mot de passe invalide' });
-                    }
+            },
+            function (userFound, bcryptedPassword, done) {
+                var newUser = models.User.create({
+                    email: email,
+                    username: username,
+                    password: bcryptedPassword,
+                    bio: bio,
+                    isAdmin: 0
+                })
+                    .then(function (newUser) {
+                        done(newUser);
+                    })
+                    .catch(function (err) {
+                        return res.status(500).json({ 'erreur': 'vous ne pouvez pas ajouter d`utlisateur' });
+                    });
+            }
+        ], function (newUser) {
+            if (newUser) {
+                return res.status(201).json({
+                    'userId': newUser.id
                 });
             } else {
-                return res.status(404).json ({ 'erreur': 'L`utilisateur n`existe pas dans la base de donnée' });
+                return res.status(500).json({ 'erreur': 'vous ne pouvez pas ajouter d`utlisateur' });
             }
-        
-        })
-        .catch(function(err) {
-            return res.status(500).json({ 'erreur': 'vous ne pouvez pas ajouter d`utlisateur' });
         });
+    },
+    login: function (req, res) {
 
+        // Params
+        var email = req.body.email;
+        var password = req.body.password;
 
+        if (email == null || password == null) {
+            return res.status(400).json({ 'erreur': 'Il manque des paramètres' });
+        }
+
+        asyncLib.waterfall([
+            function (done) {
+                models.User.findOne({
+                    where: { email: email }
+                })
+                    .then(function (userFound) {
+                        done(null, userFound);
+                    })
+                    .catch(function (err) {
+                        return res.status(500).json({ 'erreur': 'impossible de vérifier l`utilisateur' });
+                    });
+            },
+            function (userFound, done) {
+                if (userFound) {
+                    bcrypt.compare(password, userFound.password, function (errBycrypt, resBycrypt) {
+                        done(null, userFound, resBycrypt);
+                    });
+                } else {
+                    return res.status(404).json({ 'erreur': 'L`utilisateur n`existe pas dans la base de donnée' });
+                }
+            },
+            function (userFound, resBycrypt, done) {
+                if (resBycrypt) {
+                    done(userFound);
+                } else {
+                    return res.status(403).json({ 'erreur': 'Mot de passe invalide' });
+                }
+            }
+        ], function (userFound) {
+            if (userFound) {
+                return res.status(201).json({
+                    'userId': userFound.id,
+                    'token': jwtUtils.generateTokenForUser(userFound)
+                });
+            } else {
+                return res.status(500).json({ 'erreur': 'impossible de se connecter sur cet utilisateur' });
+            }
+        });
+    },
+    getUserProfile: function (req, res) {
+        // Getting auth header
+        var headerAuth = req.headers['authorization'];
+        var userId = jwtUtils.getUserId(headerAuth);
+
+        if (userId < 0)
+            return res.status(400).json({ 'erreur': 'token invalide' });
+
+        models.User.findOne({
+            attributes: ['id', 'email', 'username', 'bio'],
+            where: { id: userId }
+        }).then(function (user) {
+            if (user) {
+                res.status(201).json(user);
+            } else {
+                res.status(404).json({ 'erreur': 'utilisateur introuvable' });
+            }
+        }).catch(function (err) {
+            res.status(500).json({ 'error': 'cannot fetch user' });
+        });
+    },
+    updateUserProfile: function (req, res) {
+        // Getting auth header
+        var headerAuth = req.headers['authorization'];
+        var userId = jwtUtils.getUserId(headerAuth);
+
+        // Params
+        var bio = req.body.bio;
+
+        asyncLib.waterfall([
+            function (done) {
+                models.User.findOne({
+                    attributes: ['id', 'bio'],
+                    where: { id: userId }
+                }).then(function (userFound) {
+                    done(null, userFound);
+                })
+                    .catch(function (err) {
+                        return res.status(500).json({ 'erreur': 'impossible de vérifier l`utilisateur' });
+                    });
+            },
+            function (userFound, done) {
+                if (userFound) {
+                    userFound.update({
+                        bio: (bio ? bio : userFound.bio)
+                    }).then(function () {
+                        done(userFound);
+                    }).catch(function (err) {
+                        res.status(500).json({ 'erreur': 'l`utilisateur ne peut être modifier' });
+                    });
+                } else {
+                    res.status(404).json({ 'erreur': 'utilisateur introuvable' });
+                }
+            },
+        ], function (userFound) {
+            if (userFound) {
+                return res.status(201).json(userFound);
+            } else {
+                return res.status(500).json({ 'erreur': 'l`utilisateur ne peut être modifier' });
+            }
+        });
     }
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,4 +7,7 @@ console.log('coucou');
 router.post('/user/register', userCtrl.register);
 router.post('/user/login', userCtrl.login);
 
+router.get('/user/me', userCtrl.getUserProfile);
+router.put('/user/me', userCtrl.updateUserProfile);
+
 module.exports = router;
